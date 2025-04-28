99dd1f697055acab2e5b0d87c827adb2e172a575
abelureste <aureste03@gmail.com>
2025-04-28 14:35:25 -0400

    Fixed orientation issue

--- flaskServer.py
+++ flaskServer.py
@@ -13,6 +13,7 @@ class Data(db.Model):
     name = db.Column(db.String(200))
     file = db.Column(db.LargeBinary)
     filename = db.Column(db.String(200))
+    pic_orientation = db.Column(db.Integer)
 
     def __repr__(self):
         return f'<Data {self.name}>'
@@ -21,16 +22,13 @@ with app.app_context():
     db.create_all()
 
 def resize_and_crop(img, target_width=600, target_height=448):
-    # Step 1: Resize proportionally to cover the target area
     img_ratio = img.width / img.height
     target_ratio = target_width / target_height
 
     if img_ratio > target_ratio:
-        # Image is wider than target: fit height, crop width
         new_height = target_height
         new_width = int(new_height * img_ratio)
     else:
-        # Image is taller than target: fit width, crop height
         new_width = target_width
         new_height = int(new_width / img_ratio)
 
@@ -70,29 +68,48 @@ def index():
         try:
             register_heif_opener()
             img = Image.open(picFile.stream)
+
+            orientation = None
+            try:
+                exif = img._getexif()
+                if exif is not None:
+                    for tag, value in exif.items():
+                        decoded_tag = ExifTags.TAGS.get(tag, tag)
+                        if decoded_tag == 'Orientation':
+                            orientation = value
+                            break
+            except Exception as e:
+                print(f"EXIF reading error: {e}")
+
             img = resize_and_crop(img)
-            
+
             img_io = BytesIO()
             if img.mode != "RGB":
                 img = img.convert("RGB")
             img.save(img_io, format='JPEG', quality=90)
             img_io.seek(0)
 
-            newPic = Data(name=picName, filename=picFile.filename, file=img_io.read())
+            newPic = Data(
+                name=picName,
+                filename=picFile.filename,
+                file=img_io.read(),
+                pic_orientation=orientation
+            )
 
-        except:
-            return "Please enter a picture"
+        except Exception as e:
+            return f"Please enter a valid picture. Error: {e}"
 
         try:
             db.session.add(newPic)
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue adding your picture'
+        except Exception as e:
+            return f'There was an issue adding your picture: {e}'
 
     else:
         pictures = Data.query.order_by(Data.name).all()
         return render_template('index.html', pictures=pictures)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
